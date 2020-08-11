8fbc8be4f9826e83e17229c471d4705339506de3
mainguyenanhvu <26211591+mainguyenanhvu@users.noreply.github.com>
2020-08-11 07:56:51 +0700

    Add files via upload

---
+++ opencv.sh
@@ -0,0 +1,25 @@
+sudo apt -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt -y install git gfortran
+sudo apt -y install libjpeg8-dev libjasper-dev libpng12-dev
+ 
+sudo apt -y install libtiff5-dev
+ 
+sudo apt -y install libtiff-dev
+ 
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt -y install libxine2-dev libv4l-dev
+
+
+sudo apt -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt -y install libatlas-base-dev
+sudo apt -y install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt -y install libvorbis-dev libxvidcore-dev
+sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt -y install libavresample-dev
+sudo apt -y install x264 v4l-utils
+
+sudo apt -y install libprotobuf-dev protobuf-compiler
+sudo apt -y install libgoogle-glog-dev libgflags-dev
+sudo apt -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
